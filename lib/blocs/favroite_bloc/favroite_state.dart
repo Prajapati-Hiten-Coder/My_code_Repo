5c1306459f249b53da169bc8888e6445265afd6f
Prajapati_hiten_develper <prajapatihiten386@gmail.com>
2024-12-22 15:16:17 +0530

    Revert "adding favorite to app!"
    This reverts commit ca9316054c00bcd924e340c0f6dc449c22cc6a74.

---
+++ lib/blocs/favroite_bloc/favroite_state.dart
@@ -0,0 +1,6 @@
+part of 'favroite_bloc.dart';
+
+@immutable
+sealed class FavroiteState {}
+
+final class FavroiteInitial extends FavroiteState {}
--- lib/pages/favoritepage.dart
+++ lib/pages/favoritepage.dart
@@ -1,58 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:my_flutter_app/blocs/favorite_bloc/favorite_bloc.dart';
-import 'package:my_flutter_app/widget/productcard_fav.dart';
 
 class Favorite extends StatefulWidget {
   const Favorite({super.key});
 
   @override
-  State<Favorite> createState() => Favorite_State();
+  State<Favorite> createState() => FavoriteState();
 }
 
-class Favorite_State extends State<Favorite> {
-  final FavoriteBloc favoriteBloc = FavoriteBloc();
-  @override
-  void initState() {
-    favoriteBloc.add(FavoriteInitialEvent());
-    super.initState();
-  }
+class FavoriteState extends State<Favorite> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('favorite'),
         backgroundColor: Colors.blue,
-      ),
-      body: BlocConsumer<FavoriteBloc,FavoriteState>(
-        bloc: favoriteBloc,
-        listener: (context, state) {},
-        buildWhen:  (previous,current)=>true,
-        listenWhen: (previous,current)=>current is FavoriteActionState,
-        builder: (context, state) {
-          switch(state.runtimeType) {
-            case FavoriteSuccessState:
-              final successstate = state as FavoriteSuccessState;
-              return Scaffold(
-                body:
-                GridView.builder(
-                    itemCount: successstate.favoriteList.length,
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2),
-                    itemBuilder: (context, index) {
-                      return ProductCard_fav(
-                        product: successstate.favoriteList[index],
-                         favoriteBloc: favoriteBloc,
-                      );
-                    }
-                ),
-              );
-            default:
-              return const SizedBox();
-          }
-
-              
-        },
       ),
     );
   }
